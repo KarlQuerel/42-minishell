9c8b984d69d6a770fe66c203a26960d25416d483
Carolina SOMARRIBA <casomarr@made-f0Cr2s3.clusters.42paris.fr>
2023-11-28 21:00:10 +0100

    en cours

--- src/parsing/determine_cmd.c
+++ src/parsing/determine_cmd.c
@@ -6,7 +6,7 @@
 /*   By: casomarr <casomarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 20:35:37 by casomarr          #+#    #+#             */
-/*   Updated: 2023/11/28 20:55:58 by casomarr         ###   ########.fr       */
+/*   Updated: 2023/11/28 20:56:36 by casomarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,44 +100,3 @@ int	determine_command_type(char *line, size_t end, size_t start)
 	}
 	return (COMMAND);
 }
-
-/*Determines the type of a given cmd for the parsing function.*/
-// int	determine_command_type(char *line, size_t end, size_t start)
-// {
-// 	if (line[start] == '\'' || line[start] == '\"')
-// 		start = start + 1;
-// 	if ((line[start] == '-' && ft_isalpha(line[start + 1]) == 1) || \
-// 	(line[end] >= 4 && line[start] == '-' && (line[start + 1] == '\'' || \
-// 	line[start + 1] == '\"') && ft_isalpha(line[start + 2]) == 1 && \
-// 	(line[end - 1] == '\'' || line[end - 1] == '\"')))
-// 		return (OPTION);
-// 	if ((start >= 3 && (line[start - 1] == ' ' && line[start - 2] == '<' && line[start - 3] == '<'/*  && line[start - 4] == ' ' */)/*  && nothing_before(start, 3, line) == true */) || \
-// 	(start >= 2 && (line[start - 1] == '<' && line[start - 2] == '<'/*  && line[start - 3] == ' ' */)/*  && nothing_before(start, 4, line) == true */) || \
-// 	(start >= 3 && ((line[start - 1] == '\"' || line[start - 1] == '\'') && line[start - 2] == '<' && line[start - 3] == '<'/*  && line[start - 4] == ' ' */)/*  && nothing_before(start, 3, line) == true */) || \
-// 	(start >= 4 && ((line[start - 1] == '\"' || line[start - 1] == '\'') && line[start - 2] == ' ' && line[start - 3] == '<' && line[start - 4] == '<'/*  && line[start - 5] == ' ' */)/*  && nothing_before(start, 4, line) == true */))
-// 		return (HEREDOC);
-// 	if ((start >= 2 && (line[start - 1] == ' ' && line[start - 2] == '<'/*  && line[start - 3] == ' ' */)/*  && nothing_before(start, 2, line) == true */) || \
-// 	(start >= 1 && line[start - 1] == '<'/*  && line[start - 2] == ' ' *//*  && nothing_before(start, 1, line) == true */) || \
-// 	(start >= 2 && ((line[start - 1] == '\"' || line[start - 1] == '\'') && line[start - 2] == '<'/*  && line[start - 3] == ' ' */)/*  && nothing_before(start, 2, line) == true */) || \
-// 	(start >= 3 && ((line[start - 1] == '\"' || line[start - 1] == '\'') && line[start - 2] == ' ' && line[start - 3] == '<'/*  && line[start - 4] == ' ' */)/*  && nothing_before(start, 3, line) == true */))
-// 		return (INFILE);
-// 	if ((start >= 3 && (line[start - 1] == ' ' && line[start - 2] == '>' && line[start - 3] == '>'/*  && line[start - 4] == ' ' */)/*  && nothing_before(start, 3, line) == true */) || \
-// 	(start >= 2 && (line[start - 1] == '>' && line[start - 2] == '>'/*  && line[start - 3] == ' ' */)/*  && nothing_before(start, 2, line) == true */) || \
-// 	(start >= 3 && ((line[start - 1] == '\"' || line[start - 1] == '\'') && line[start - 2] == '>' && line[start - 3] == '>'/*  && line[start - 4] == ' ' */)/*  && nothing_before(start, 3, line) == true */) || \
-// 	(start >= 4 && ((line[start - 1] == '\"' || line[start - 1] == '\'') && line[start - 2] == ' ' && line[start - 3] == '>' && line[start - 4] == '>'/*  && line[start - 5] == ' ' */)/*  && nothing_before(start, 4, line) == true */))
-// 		return (OUTFILE_APPEND);
-// 	if ((start >= 2 && (line[start - 1] == ' ' && line[start - 2] == '>'/*  && line[start - 3] == ' ' */)/*  && nothing_before(start, 2, line) == true */) || \
-// 	(start >= 1 && line[start - 1] == '>'/*  && line[start - 1] == ' ' *//*  && nothing_before(start, 1, line) == true */) || \
-// 	(start >= 2 && ((line[start - 1] == '\"' || line[start - 1] == '\'') && line[start - 2] == '>'/*  && line[start - 3] == ' ' */)/*  && nothing_before(start, 2, line) == true */) || \
-// 	(start >= 3 && ((line[start - 1] == '\"' || line[start - 1] == '\'') && line[start - 2] == ' ' && line[start - 3] == '>'/*  && line[start - 4] == ' ' */)/*  && nothing_before(start, 3, line) == true */))
-// 		return (OUTFILE);
-// 	if (ft_strncmp(&line[start], "|", 1) == 0)
-// 		return (PIPE);
-// 	while(start < end)
-// 	{
-// 		if (line[start] == ' ')
-// 			return (ARGUMENT);
-// 		start++;
-// 	}
-// 	return (COMMAND);
-// }
